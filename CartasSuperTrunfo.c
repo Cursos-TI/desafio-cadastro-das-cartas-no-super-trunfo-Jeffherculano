2eb2c07ff9d495f1fffe27d86c161c221179d4b9
Jefferson Herculano <167326365+Jeffherculano@users.noreply.github.com>
2025-05-13 22:27:05 +0000

    Declaração de variáveis da primeira carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,5 +3,16 @@
 int main () {
     printf("Desafio de nível novato baseado na aula de fundamentos em C \n ");
 
+    //Declaração de variáveis da primeira carta:
     
+    char estado_1;
+    char codcarta_1[5];
+    char nome_cidade1[20];
+    int população1;
+    float areakm1;
+    float pib1;
+    int ponto_tur1;
+
+    
+
 }
