694bb1f747e3518c1c96f122cdeb7ca86b58f3e5
Jefferson Herculano <167326365+Jeffherculano@users.noreply.github.com>
2025-05-19 22:18:25 +0000

    Término de calculo de super poder.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,35 +1,29 @@
 #include <stdio.h>
 
+float calcula_super(int a,float b,float c,int d,float e,float f) {
+    float resultado = a + b + c + d + e +(1 / f);
+    return resultado;
+}
+
 int main () {
-    printf("Desafio de nível novato baseado na aula de fundamentos em C \n ");
+    printf("Trabalho de Super Trunfo em C, trabalhando a inserção e visualização de dados, bem como operações matemáticas e comparação de valores. \n ");
 
-    //Declaração de variáveis da primeira carta:
+    //Declaração de variáveis da primeira e segunda carta:
     
-    char estado_1;
-    char codcarta_1[10];
-    char nome_cidade1[20];
-    int popul1;
-    float areakm1;
-    float pib1;
-    int ponto_tur1;
-    float denspop1 = (float) popul1 / areakm1;
-    float pibcpt1 = (float) pib1 / popul1;
-
-    //Declaração de variáveis da segunda carta:
-
-    char estado_2;
-    char codcarta_2[10];
-    char nome_cidade2[20];
-    int popul2;
-    float areakm2;
-    float pib2;
-    int ponto_tur2;
-    float denspop2 = (float) popul2 / areakm2;
-    float pibcpt2 = (float) pib2 / popul2;
+    char estado_1, estado_2;
+    char codcarta_1[10], codcarta_2[10];
+    char nome_cidade1[20], nome_cidade2[20];
+    int popul1, popul2;
+    float areakm1, areakm2;
+    float pib1, pib2;
+    int ponto_tur1, ponto_tur2;
+    float denspop1, denspop2;
+    float pibcpt1, pibcpt2;
+    float super1, super2;
 
     // Inserção de dados da primeira carta:
 
-    printf("Agora você irá inserir os dados da primeira carta. \n");
+    printf("Por favor, insira os dados da primeira carta. \n");
     
     printf("Digite a primeira letra do estado: ");
     scanf("%c", &estado_1);
@@ -53,10 +47,11 @@ int main () {
     printf("Digite a quantidade de pontos turísticos nesta cidade: ");
     scanf("%d", &ponto_tur1);
 
-// Preciso inserir a declaração e o cálculo da densidade populacional e do PIB per Capita aqui, se não da erro.
+// Calculo de densidade, PIB per capita e super poder da primeira carta.
 
-    float denspop1 = (float)popul1 / areakm1;
-    float pibcpt1 = (pib1 * 1e9) / (float)popul1; //Aqui acontece a conversão de bilhões para reais.
+    denspop1 = (float)popul1 / areakm1;
+    pibcpt1 = (pib1 * 1e9) / (float)popul1; //Aqui acontece a conversão de bilhões para reais.
+    super1 = calcula_super(popul1, areakm1, pib1, ponto_tur1, pibcpt1, denspop1);
 
 // As próximas linhas apenas mostram os dados da primeira carta que foram inseridos, além da densidade populacional e PIB per Capita.
 
@@ -64,10 +59,11 @@ int main () {
     printf("Estado: %c \n O código da carta: %s \n O nome da cidade: %s", estado_1, codcarta_1, nome_cidade1);
     printf("A população da cidade é: %d \n A área desta cidade em quilômetros é: %.2fkm² \n", popul1, areakm1);
     printf("O PIB desta cidade é: %.2f Bilhões de reais. \n A quantidade de pontos turísticos nesta cidade é: %i \n", pib1, ponto_tur1);
-    printf("A densidade populacional da cidade é: %.2f hab/km². \n O PIB per Capita da cidade é: %.2f reais.", denspop1, pibcpt1);
+    printf("A densidade populacional da cidade é: %.2f hab/km². \n O PIB per Capita da cidade é: %.2f reais. \n", denspop1, pibcpt1);
+    printf("Resultado do calculo de super poder: %.1f \n", super1);
 
 
-// Agora será a inserção de dados da segunda carta.
+//  Agora será a inserção de dados da segunda carta.
 
     printf("Agora você irá inserir os dados da segunda carta. \n");
     getchar();
@@ -94,11 +90,11 @@ int main () {
     printf("Digite a quantidade de pontos turísticos nesta cidade: ");
     scanf("%d", &ponto_tur2);
 
-// Mesmo caso la de cima, agora pra segunda carta
+// Aqui acontece o calculo da densidade, PIB per capita e super poder da segunda carta
 
-   float denspop2 = (float)popul2 / areakm2;
-   float pibcpt2 = (pib2 * 1e9) / (float)popul2;
-
+   denspop2 = (float)popul2 / areakm2;
+   pibcpt2 = (pib2 * 1e9) / (float)popul2;
+   super2 = calcula_super(popul2, areakm2, pib2, ponto_tur2, pibcpt2, denspop2);
 
 // Agora será a visualização de dados da segunda carta, além da densidade populacional e PIB per Capita.
     
@@ -106,7 +102,8 @@ int main () {
     printf("Estado: %c \n O código da carta: %s \n O nome da cidade: %s", estado_2, codcarta_2, nome_cidade2);
     printf("A população da cidade é: %d \n A área desta cidade em quilômetros é: %.2fkm² \n", popul2, areakm2);
     printf("O PIB desta cidade é: %.2f Bilhões de reais. \n A quantidade de pontos turísticos nesta cidade é: %i", pib2, ponto_tur2);
-    printf("A densidade populacional da cidade é: %.2f hab/km². \n O PIB per Capita da cidade é: %.2f reais.", denspop1, pibcpt1);   
-
+    printf("A densidade populacional da cidade é: %.2f hab/km². \n O PIB per Capita da cidade é: %.2f reais. \n", denspop2, pibcpt2);   
+    printf("O super poder desta carta é: %.2f \n", super2);
     return 0;
+    
 }
